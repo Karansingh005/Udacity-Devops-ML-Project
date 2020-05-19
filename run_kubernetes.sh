4038fb24f6fbf359ca6c5c0126fd9c91eb207e21
Karan Singh <karansinghreen@gmail.com>
2020-05-19 12:15:07 +0530

    Updated run_kubernetes.sh
    Added commands for running Kubernetes pods

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="karansingh005/demolocal"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deployment kubedemo --image=$dockerpath
+
+kubectl run "kubedemo" --generator=run-pod/v1 --image=$dockerpath --port=80 --labels app="kubedemo"
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward kubedemo 8000:80
